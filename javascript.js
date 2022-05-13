6398d3e87acf5bee11bcaff1a8dd780ff3ea045e
shiela <sheshemarie31@gmail.com>
2022-05-13 21:58:25 +0800

    Move mouseover event and create resize button logic Whenever I change the size of the grid, the fill color event doesnt apply on the newly added tile. I put a range on numbers on the prompt to restrict users from inputting numbers less than one and more than a hundred and added a while loop to keep asking for an input number until the user gives the number within the given range.

--- javascript.js
+++ javascript.js
@@ -15,3 +15,28 @@ const gridContainer = document.querySelector('#grid-container');
 
     //call function
     createGrid(16, 16);
+
+    //resize grid
+    const resizeBtn = document.querySelector('#resize-btn');
+    resizeBtn.addEventListener('click', () => {
+      //input new number of squares per side
+      let userInput = parseInt(prompt('input number from 1 - 100 to generate new size of grid', 16));
+      //if cancel button from prompt is clicked
+      if(userInput === null) {
+        return;
+      } else if(userInput < 100 || userInput > 1) {
+        removeGrid()
+        makeGrid(userInput, userInput);
+      }
+      //loop prompt til given number within range
+      while(userInput > 100 || userInput < 1 || isNaN(userInput)){
+        userInput = parseInt(prompt('please input numbers only from 1 - 100', 16))
+        if(userInput === null) {
+          return;
+        } else {
+          removeGrid();
+          makeGrid(userInput, userInput);
+        }
+      }
+    })
+    
